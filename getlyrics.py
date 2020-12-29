9ee25fa9508e7fad295fb5670239d5b1911c39fd
Tom <tom.sundberg@hotmail.com>
2020-12-29 21:51:34 +0200

    Minor changes

--- getlyrics.py
+++ getlyrics.py
@@ -34,17 +34,28 @@ def lyrics(url):
     global oldUrl
     condition = True
     filepath = cache + "/" + url.removeprefix("https://www.genius.com/")
+    oldUrl = url # Set oldUrl as current url so that we don't reload the same lyrics
 
     if path.isfile(filepath): # We won't reaload the same lyrics if they're already stored in cache
+
         print(f"Loaded lyrics from {filepath} \n")
         file = open(filepath)
         contents = file.read()
         print(contents)
 
     else: # We'll have to load the lyrics if we haven't done it to this particular song yet
+
         print(f"Loading lyrics from {url} \n")
+
         while condition: # Sometimes Genius won't load up the page correctly, so we'll load the page as many times as necessary
 
+            if hasSongChanged() == True: # This is my half-ass attempt to load the correct lyrics if the user has changed the song while Genius is acting up 
+                
+                system('clear')
+                url = createUrl()
+                oldUrl = url
+                print(f"Song has changed, loading lyrics from {url} \n")
+                
             page = http.request('GET',url)
             soup = BeautifulSoup (page.data, 'html.parser')
             condition = soup.find("div", {"id": "lyrics"}) is not None
@@ -52,9 +63,7 @@ def lyrics(url):
 
         print(soup.p.get_text()) # Prints the lyrics
         file = open(filepath, "w+") # Makes and opens a file
-        file.write(soup.p.get_text()) # Writes the lyrics to the file
-
-    oldUrl = url # Set oldUrl as current url so that we don't reload the same lyrics
+        file.write(soup.p.get_text()) # Writes the lyrics to the file   
 
 
 # Creates the URL with the help of urlify
@@ -67,6 +76,7 @@ def createUrl():
             metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata") # Get metadata
             url = "https://www.genius.com/" + urlify(metadata.get('xesam:artist')[0] + " " + metadata.get('xesam:title')) # Create the URL
             sys.stdout.write("\33]0;%s - %s\a" % (metadata.get('xesam:artist')[0], metadata.get('xesam:title'))) # Change the terminal title to 'Artist - Title'
+
     except dbus.exceptions.DBusException:
 
         print("Spotify is not running.")
@@ -74,22 +84,32 @@ def createUrl():
 
     return url
 
+# Function to check wheter the song has changed
+def hasSongChanged():
+
+    url = createUrl()
+
+    if url != oldUrl:
+        
+        return True
+    
+    return False
+
 # Main
 def main():
 
     
-    if not os.path.exists(cache):
-        os.makedirs(cache)
+    if not os.path.exists(cache): # If cache folder does not exist..
+        os.makedirs(cache) # then create it
+
     try:
 
         while True:
 
-            url = createUrl()
-
-            if url != oldUrl:
+            if hasSongChanged() == True:
 
                 system('clear')
-                lyrics(url)
+                lyrics(createUrl())
 
             time.sleep(1)
 
